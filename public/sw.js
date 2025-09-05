bcd7636f91610803ba739bedbf58ce1ed0e2b09a
jaisheelan993 <thomasjaison35@gmail.com>
2025-09-05 21:16:04 +0530

    Delete public directory

--- public/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'calc-hub-v2';
-const toCache = ['/','/offline.html'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(toCache))); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
